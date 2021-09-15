6134fe8970ffec244d92984a6d337ceb41896f34
jholt1 <jonathan.holt@centrica.com>
2021-09-15 16:10:51 +0100

    chore: split and rename

---
+++ packages/library/scripts/rollup-plugins.mjs
@@ -0,0 +1,29 @@
+import { fromRollup } from '@web/dev-server-rollup';
+import stylesPlugin from 'rollup-plugin-styles';
+import injectProcessEnv from 'rollup-plugin-inject-process-env';
+import autoprefixer from 'autoprefixer';
+import postcssPreset from 'postcss-preset-env';
+import postcssImport from 'postcss-import';
+import postcssCustomProperties from 'postcss-custom-properties';
+
+const styles = fromRollup(stylesPlugin);
+const processEnv = fromRollup(injectProcessEnv);
+
+export default [
+  processEnv(
+    {
+      MUON_PREFIX: 'muon'
+    },
+    {
+      exclude: '**/*.css'
+    }
+  ),
+  styles({
+    plugins: [
+      postcssImport(),
+      postcssCustomProperties({ preserve: false }),
+      postcssPreset({ stage: 0 }),
+      autoprefixer({ grid: true, overrideBrowserslist: ['last 2 versions'] })
+    ]
+  })
+];
--- packages/library/web-test-runner.config.mjs
+++ packages/library/web-test-runner.config.mjs
@@ -1,35 +1,13 @@
-import stylesPlugin from 'rollup-plugin-styles';
-import { fromRollup } from '@web/dev-server-rollup';
-import injectProcessEnv from 'rollup-plugin-inject-process-env';
-import autoprefixer from 'autoprefixer';
-import postcssPreset from 'postcss-preset-env';
-import postcssImport from 'postcss-import';
-
-const styles = fromRollup(stylesPlugin);
-const processEnv = fromRollup(injectProcessEnv);
+import plugins from '@muon/library/scripts/rollup-plugins.mjs';
 
 // process.env.MUON_PREFIX = 'testing';
 
 export default {
   nodeResolve: true,
   mimeTypes: {
-    '**/*.css': 'js',
+    '**/*.css': 'js'
   },
   plugins: [
-    processEnv(
-      {
-        MUON_PREFIX: 'muon',
-      },
-      {
-        exclude: '**/*.css'
-      }
-    ),
-    styles({
-      plugins: [
-        postcssImport(),
-        postcssPreset({ stage: 0 }),
-        autoprefixer({ grid: true, overrideBrowserslist: ['last 2 versions'] })
-      ]
-    })
-  ],
+    ...plugins
+  ]
 };
