1fa1e93417d1d683d30ddd0488afc409d3e635d3
jholt1 <jonathan.holt@centrica.com>
2021-11-02 14:06:17 +0000

    build: break up serve script into parts for analysis and config

--- packages/library/scripts/style-dictionary-create.mjs
+++ packages/library/scripts/style-dictionary-create.mjs
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
 
+import { getConfig } from './get-config.mjs';
 import styleConfig from './style-dictionary.mjs';
 import colorTransform from '../tokens/utils/transforms/color.js';
 import stringTransform from '../tokens/utils/transforms/string.js';
@@ -11,36 +12,34 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-let config = {};
+const styleDictionary = async () => {
+  const config = await getConfig();
 
-try {
-  config = JSON.parse(fs.readFileSync('muon.config.json').toString());
-}
-catch (e) {
-  console.error('Missing config, is this the right folder?', e);
-  process.exit(1);
-}
+  // Set the overriding tokens if there are any
+  if (config.tokens && config.tokens.dir) {
+    styleConfig.source = config.tokens.dir;
+  }
 
-// Set the overriding tokens if there are any
-if (config.tokens && config.tokens.dir) {
-  styleConfig.source = config.tokens.dir;
-}
+  const tokenUtils = path.join(__dirname, '..', 'tokens', 'utils');
+  const cssFontTemplate = _.template(fs.readFileSync(path.join(tokenUtils, 'templates', 'font-face.css.template')));
 
-const tokenUtils = path.join(__dirname, '..', 'tokens', 'utils');
-const cssFontTemplate = _.template(fs.readFileSync(path.join(tokenUtils, 'templates', 'font-face.css.template')));
+  const styleDict = StyleDictionary.extend(styleConfig);
 
-const styleDictionary = StyleDictionary.extend(styleConfig);
+  styleDict.registerFormat({
+    name: 'css/fonts',
+    formatter: cssFontTemplate
+  });
 
-styleDictionary.registerFormat({
-  name: 'css/fonts',
-  formatter: cssFontTemplate
-});
+  styleDict.registerTransform(stringTransform);
+  styleDict.registerTransform(colorTransform);
 
-styleDictionary.registerTransform(stringTransform);
-styleDictionary.registerTransform(colorTransform);
+  return styleDict;
+};
 
 const createTokens = async () => {
-  await styleDictionary.buildAllPlatforms();
+  const dictionary = await styleDictionary();
+
+  return dictionary.buildAllPlatforms();
 };
 
 export {
