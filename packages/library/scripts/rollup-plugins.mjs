3a0b65da120d1e3d1936e229971f33238e461a49
jholt1 <jonathan.holt@centrica.com>
2021-11-22 15:15:10 +0000

    build: use simple-var for tokens

--- packages/library/scripts/rollup-plugins.mjs
+++ packages/library/scripts/rollup-plugins.mjs
@@ -4,7 +4,8 @@ import replacePlugin from '@rollup/plugin-replace';
 import autoprefixer from 'autoprefixer';
 import postcssPreset from 'postcss-preset-env';
 import postcssImport from 'postcss-import';
-import postcssCustomProperties from 'postcss-custom-properties';
+import postcssVariables from 'postcss-simple-vars';
+import * as variables from '../build/tokens/es6/muon-tokens.mjs';
 
 const styles = fromRollup(stylesPlugin);
 const replace = fromRollup(replacePlugin);
@@ -18,8 +19,8 @@ export default [
   }),
   styles({
     plugins: [
+      postcssVariables({ variables }),
       postcssImport(),
-      postcssCustomProperties({ /*preserve: false*/ }),
       postcssPreset({ stage: 0 }),
       autoprefixer({ grid: true, overrideBrowserslist: ['last 2 versions'] })
     ]
