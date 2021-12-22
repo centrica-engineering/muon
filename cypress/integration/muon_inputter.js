b9dfc849d314bab3e3bd43cb820bd3a8b5e72ed9
PraveenAsokan <Praveen.Asokan@centrica.com>
2021-12-22 13:50:48 +0000

    feat: custom commands

--- cypress/integration/muon_inputter.js
+++ cypress/integration/muon_inputter.js
@@ -7,8 +7,8 @@ describe('muon-inputter component', () => {
   
   
     it('Should find and enter the input in inputter', () => {
-      cy.get('muon-inputter').find('input').clear()
-      cy.get('muon-inputter').find('input').type('Random text')
+      cy.clearInput()
+      cy.enterValue('Random text')
       cy.get('muon-inputter').find('input').should('have.value','Random text')
       cy.get('muon-inputter').find('label').should('have.text','A label')
       cy.get('muon-inputter').shadow().find('div[class=" input-holder is-dirty "]').should('be.visible');
@@ -16,8 +16,13 @@ describe('muon-inputter component', () => {
   
     it('Should validate the validation message', () => {
       cy.get('muon-inputter').invoke('attr','validation').should('eq','["isRequired","minLength(6)"]')
-      cy.get('muon-inputter').find('input').type('chop')
-    //   cy.get('muon-inputter').shadow().find('div[class=" input-holder is-dirty "]').find('div[class="validation"]').should('have.text','Length must be at least 6 characters.');
+      cy.clearInput()
+      cy.enterValue('chop{enter}')
+      cy.validateMessage('Length must be at least 6 characters.');
+      cy.clearInput()
+      cy.enterValue('{enter}')
+      cy.validateMessage('This field is required.');
+      
     });
   
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,18 @@
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
+
+Cypress.Commands.add('clearInput', () => {
+    cy.get('muon-inputter').find('input').clear()
+})
+
+Cypress.Commands.add('enterValue',(value) => {
+    cy.get('muon-inputter').find('input').type(value)
+})
+
+Cypress.Commands.add('validateMessage',(validationMessage) => {
+    cy.get('muon-inputter').shadow().find('div[class="validation"]').contains(validationMessage);
+})
 //
 // For more comprehensive examples of custom
 // commands please read more here:
