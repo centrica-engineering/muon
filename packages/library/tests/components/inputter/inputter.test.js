b51e46e5924719b90ab160dc657138ab216bc47a
MekalaNagarajan-Centrica <mekala.nagarajan@centrica.com>
2022-01-05 15:04:51 +0000

    fix: test failures

--- packages/library/tests/components/inputter/inputter.test.js
+++ packages/library/tests/components/inputter/inputter.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { expect, fixture, html, defineCE, unsafeStatic } from '@open-wc/testing';
+import { expect, fixture, html, defineCE, unsafeStatic, waitUntil } from '@open-wc/testing';
 import { Inputter } from '@muons/library/components/inputter';
 import { defaultChecks, fillIn } from '../../helpers';
 
@@ -35,6 +35,7 @@ describe('Inputter', () => {
 
     it('input value `1`', async () => {
       await fillIn(inputElement, '1', 'input');
+      await waitUntil(() => inputter.value === '1');
       maskedInput = shadowRoot.querySelector('.input-mask');
       expect(inputter.value).to.be.equal('1', 'Inputter has correct value');
       expect(maskedInput.textContent).to.be.equal(' 0000', '`input-mask` has correct value');
@@ -42,6 +43,7 @@ describe('Inputter', () => {
 
     it('input value `12`', async () => {
       await fillIn(inputElement, '12', 'input');
+      await waitUntil(() => inputter.value === '12');
       maskedInput = shadowRoot.querySelector('.input-mask');
       expect(inputter.value).to.be.equal('12', 'Inputter has correct value');
       expect(maskedInput.textContent).to.be.equal('  000', '`input-mask` has correct value');
@@ -75,6 +77,7 @@ describe('Inputter', () => {
 
     it('input value `1`', async () => {
       await fillIn(inputElement, '1', 'input');
+      await waitUntil(() => inputter.value === '1');
       maskedInput = inputter.shadowRoot.querySelector('.input-mask');
       expect(inputter.value).to.be.equal('1', 'Inputter has correct value');
       expect(maskedInput.textContent).to.be.equal(' 0-00-00', '`input-mask` has correct value');
@@ -82,9 +85,34 @@ describe('Inputter', () => {
 
     it('input value `12`', async () => {
       await fillIn(inputElement, '12', 'input');
+      await waitUntil(() => inputter.value === '12-');
       maskedInput = inputter.shadowRoot.querySelector('.input-mask');
       expect(inputter.value).to.be.equal('12-', 'Inputter has correct value');
       expect(maskedInput.textContent).to.be.equal('   00-00', '`input-mask` has correct value');
+    });
+
+    it('input value `123`', async () => {
+      await fillIn(inputElement, '123', 'input');
+      await waitUntil(() => inputter.value === '12-3');
+      maskedInput = inputter.shadowRoot.querySelector('.input-mask');
+      expect(inputter.value).to.be.equal('12-3', 'Inputter has correct value');
+      expect(maskedInput.textContent).to.be.equal('    0-00', '`input-mask` has correct value');
+    });
+
+    it('delete value `3`', async () => {
+      await fillIn(inputElement, '1', 'input');
+      await waitUntil(() => inputter.value === '1');
+      maskedInput = inputter.shadowRoot.querySelector('.input-mask');
+      expect(inputter.value).to.be.equal('1', 'Inputter has correct value');
+      expect(maskedInput.textContent).to.be.equal(' 0-00-00', '`input-mask` has correct value');
+    });
+
+    it('input value `123`', async () => {
+      await fillIn(inputElement, '123');
+      await waitUntil(() => inputter.value === '12-3');
+      maskedInput = inputter.shadowRoot.querySelector('.input-mask');
+      expect(inputter.value).to.be.equal('12-3', 'Inputter has correct value');
+      expect(maskedInput.textContent).to.be.equal('    0-00', '`input-mask` has correct value');
     });
   });
 });
