28a15d335d858f5ee00a38c3e476ac4597551a45
jholt1 <jonathan.holt@centrica.com>
2022-01-06 14:15:58 +0000

    refactor: make format easier to read

--- packages/library/mixins/mask-mixin.js
+++ packages/library/mixins/mask-mixin.js
@@ -93,18 +93,23 @@ export const MaskMixin = dedupeMixin((superclass) =>
       const parts = this.mask.split(this.separator);
       let processedValue = '';
       let length = 0;
-      let partsLength = 0;
+      let currentLength = 0;
+
       for (let i = 0; i < parts.length && length < formattedValue.length; i++) {
-        partsLength += parts[i].length;
         const remainingLength = formattedValue.length - length;
-        processedValue += formattedValue.substr(
-          length, remainingLength > parts[i].length ? parts[i].length : remainingLength);
-        length += parts[i].length;
-        if (i < (parts.length - 1) && processedValue.length === partsLength) {
+        const splitPoint = remainingLength > parts[i].length ? parts[i].length : remainingLength;
+
+        processedValue += formattedValue.substr(length, splitPoint);
+        currentLength += parts[i].length;
+
+        if (i < (parts.length - 1) && processedValue.length === currentLength) {
           processedValue += this.separator;
-          partsLength += 1;
+          currentLength += 1;
         }
+
+        length += parts[i].length;
       }
+
       return processedValue;
     }
 
