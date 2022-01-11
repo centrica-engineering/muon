9e11667b946f1a3e3ff7aa24d52a9e7b3e9d99e1
jholt1 <jonathan.holt@centrica.com>
2022-01-11 09:57:52 +0000

    feat: token storybook

---
+++ packages/library/storybook/tokens/spacer.js
@@ -0,0 +1,48 @@
+import tokens from '/dist/tokens/es6/muon-tokens-module.js';
+
+export default {
+  title: 'Tokens/Theme'
+};
+
+const spacerHTML = (key, data) => {
+  const size = data.value;
+  return `
+    <h2>${key}</h2>
+    <span>${size}</span>
+    <div style="width: ${size}; height: ${size};"  class="square"></div>
+  `;
+};
+
+const generateAll = () => {
+  return Object.entries(tokens.theme.spacer).map((entry) => {
+    const key = entry[0];
+    const data = entry[1];
+
+    if (data.value) {
+      return spacerHTML(key, data);
+    }
+
+    return undefined;
+  }).filter((entry) => entry).join('');
+};
+
+export const spacer = () => {
+  return `<div class="inner" type="page">
+  <style>
+    body {
+      padding: 2em;
+    }
+
+    h2 {
+      text-transform: uppercase;
+    }
+
+    .square {
+      background: #888;
+      border: 2px solid #FFF;
+    }
+  </style>
+  <h1>Spacers</h1>
+    ${generateAll()}
+</div>`;
+};
