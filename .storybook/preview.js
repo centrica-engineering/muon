b245cbe282b5bb4851adddbb88535d8c94623ed2
jholt1 <jonathan.holt@centrica.com>
2022-01-12 16:05:39 +0000

    feat: deploy storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,9 @@
 import { setCustomElements } from '@web/storybook-prebuilt/web-components';
+import isChromatic from "chromatic/isChromatic";
 
+const url = isChromatic() ? 'custom-elements.json' : '../dist/custom-elements.json';
 const customElements = await (
-  await fetch(new URL('../dist/custom-elements.json', import.meta.url))
+  await fetch(url)
 ).json();
 
 setCustomElements(customElements);
---
+++ packages/library/scripts/build-storybook.mjs
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import { exec } from 'child_process';
+import path from 'path';
+import fs from 'fs';
+
+import { createTokens } from './style-dictionary-create.mjs';
+import { createComponentElementsJson } from './custom-elements-json.mjs';
+
+import postcss from 'postcss';
+import { postcssPlugins } from './rollup-plugins.mjs';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const globalCSSUrl = path.join(__filename, '..', '..', 'css', 'global.css');
+
+// @TODO: make reusable
+const createGlobalCSS = async (destination) => {
+  const globalCSSDest = path.join(destination, 'muon.min.css');
+  const globalCSS = await fs.readFileSync(globalCSSUrl);
+  const processedCSS = await postcss(postcssPlugins).process(globalCSS, { from: globalCSSUrl, to: globalCSSDest });
+
+  fs.writeFileSync(globalCSSDest, processedCSS.css, 'utf8');
+};
+
+const main = async () => {
+  const outputDir = 'storybook-static';
+
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+
+  await createTokens();
+  await createComponentElementsJson(outputDir);
+  await createGlobalCSS(outputDir);
+
+  exec('build-storybook');
+};
+
+main();
