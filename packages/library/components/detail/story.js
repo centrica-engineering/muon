4525c0db91ed9ae21c5cfd8ec01be8811ea576d6
Drew Jones <drew.jones@centrica.com>
2022-01-12 14:04:57 +0000

    feat: update stories

--- packages/library/components/detail/story.js
+++ packages/library/components/detail/story.js
@@ -2,14 +2,23 @@ import { Detail } from '@muons/library/components/detail';
 import setup from '@muons/library/storybook/stories';
 
 const details = setup('detail', Detail);
+
 export default details.defaultValues;
 
 const innerDetail = (args) => `
-<div slot="heading">${args.heading}</div>
-${args.content}
+  <div slot="heading">${args.heading}</div>
+  ${args.content}
 `;
+
 export const Standard = (args) => details.template(args, innerDetail);
-Standard.args = { icon: '', heading: 'Can I manage my account online?', content: 'Yes, with an online account you can arrange a service visit, find out whatʼs happening with your appointment, submit a meter reading and book an engineer. Weʼve even got a free smartphone app.' };
+Standard.args = {
+  heading: 'Can I manage my account online?',
+  content: 'Yes, with an online account you can arrange a service visit, find out whatʼs happening with your appointment, submit a meter reading and book an engineer. Weʼve even got a free smartphone app.'
+};
 
 export const WithIcon = (args) => details.template(args, innerDetail);
-WithIcon.args = { icon: 'dot-circle', heading: 'Can I manage my account online?', content: 'Yes, with an online account you can arrange a service visit, find out whatʼs happening with your appointment, submit a meter reading and book an engineer. Weʼve even got a free smartphone app.' };
+WithIcon.args = {
+  icon: 'dot-circle',
+  heading: 'Can I manage my account online?',
+  content: 'Yes, with an online account you can arrange a service visit, find out whatʼs happening with your appointment, submit a meter reading and book an engineer. Weʼve even got a free smartphone app.'
+};
--- packages/library/components/inputter/story.js
+++ packages/library/components/inputter/story.js
@@ -5,61 +5,177 @@ const details = setup('inputter', Inputter);
 
 export default details.defaultValues;
 
-const innerInputText = (args) => `
+const labelTemplate = (args) => `
   <label slot="label">${args.label}</label>
-  <input type="text" value="${args.value}" placeholder="Placeholder" disabled>
-  <div slot="tip-details">By providing clarification on why this information is necessary.</div>
 `;
 
-export const Standard = (args) => details.template(args, innerInputText);
-Standard.args = { label: 'A label', value: '', helper: 'How can we help you? ', validation: '[&quot;isRequired&quot;,&quot;minLength(6)&quot;]' };
-
-const choiceInputText = (args) => `
-  <input type="${args.inputtype}" name="question" value="gas"></input>
-  <label for="gas">Gas</label>
-  <input type="${args.inputtype}" name="question" value="electricity"></input>
-  <label for="electricity">Electricity</label>
-  <div slot="tip-details">By providing clarification on why this information is necessary.</div>
+const tipDetailsTemplate = (args) => `
+  <div slot="tip-details">${args.tip}</div>
 `;
-export const Radio = (args) => details.template(args, choiceInputText);
-Radio.args = { inputtype: 'radio', value: '', heading: 'What is your heating source?', helper: 'How can we help you? ', validation: '[&quot;isRequired&quot;]' };
 
-export const Checkbox = (args) => details.template(args, choiceInputText);
-Checkbox.args = { inputtype: 'checkbox', value: '', heading: 'What is your heating source?', helper: 'How can we help you? ', validation: '[&quot;isRequired&quot;]' };
+const innerText = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}" value="${args.value}" placeholder="${args.placeholder}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
 
-const selectInputText = (args) => `
-<label slot="label" for="select-input">${args.label}</label>
-  <select name="select" id="select-input">
-    <option value="">Please Select</option>
-    <option value="value-01">One</option>
-    <option value="value-02">Two</option>
-    <option value="value-03">Three</option>
-    <option value="value-04">Four</option>
+export const Text = (args) => details.template(args, innerText);
+Text.args = {
+  inputtype: 'text',
+  label: 'Text',
+  value: '',
+  helper: 'How can we help you?',
+  tip: 'By providing clarification on why this information is necessary.',
+  validation: '[&quot;isRequired&quot;]',
+  placeholder: 'Placeholder'
+};
+
+const innerEmail = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}" value="${args.value}" placeholder="${args.placeholder}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Email = (args) => details.template(args, innerEmail);
+Email.args = {
+  inputtype: 'email',
+  label: 'Email',
+  value: '',
+  helper: 'How can we help you?',
+  tip: 'By providing clarification on why this information is necessary.',
+  validation: '[&quot;isRequired&quot;,&quot;isEmail&quot;]',
+  placeholder: 'email@address.com'
+};
+
+const innerTel = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}" value="${args.value}" placeholder="${args.placeholder}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Tel = (args) => details.template(args, innerTel);
+Tel.args = {
+  inputtype: 'tel',
+  label: 'Tel',
+  value: '',
+  helper: 'How can we help you?',
+  tip: 'By providing clarification on why this information is necessary.',
+  validation: '[&quot;isRequired&quot;]',
+  placeholder: '01234888777'
+};
+
+const innerSearch = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Search = (args) => details.template(args, innerSearch);
+Search.args = {
+  inputtype: 'search',
+  label: 'Search'
+};
+
+const innerPassword = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Password = (args) => details.template(args, innerPassword);
+Password.args = {
+  inputtype: 'password',
+  label: 'Password'
+};
+
+const innerDate = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+export const Date = (args) => details.template(args, innerDate);
+Date.args = {
+  inputtype: 'date',
+  label: 'Date',
+  value: '',
+  validation: '[&quot;isRequired&quot;,&quot;minDate(\'01/01/2022\')&quot;]'
+};
+
+const innerNumber = (args) => `
+  ${args.label ? labelTemplate(args) : ''}
+  <input type="${args.inputtype}">
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Number = (args) => details.template(args, innerNumber);
+Number.args = {
+  inputtype: 'number',
+  label: 'Number'
+};
+
+const innerTextarea = (args) => `
+  <label slot="label">${args.label}</label>
+  <textarea placeholder="${args.placeholder}">${args.value}</textarea>
+`;
+
+export const Textarea = (args) => details.template(args, innerTextarea);
+Textarea.args = {
+  label: 'Textarea',
+  value: '',
+  validation: '[&quot;isRequired&quot;]',
+  placeholder: 'Provide information'
+};
+
+const innerCheckbox = (args) => `
+  <input type="checkbox" name="checkboxes" value="a" id="check-01"></input>
+  <label for="check-01">Option A</label>
+  <input type="checkbox" name="checkboxes" value="b" id="check-02"></input>
+  <label for="check-02">Option B</label>
+  <input type="checkbox" name="checkboxes" value="c" id="check-03"></input>
+  <label for="check-03">Option C</label>
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Checkbox = (args) => details.template(args, innerCheckbox);
+Checkbox.args = {
+  heading: 'What options do you like?',
+  helper: 'How can we help you?',
+  tip: 'By providing clarification on why this information is necessary.',
+  validation: '[&quot;isRequired&quot;]'
+};
+
+const innerRadio = (args) => `
+  <input type="radio" name="radiobuttons" value="a" id="radio-01"></input>
+  <label for="radio-01">Choice A</label>
+  <input type="radio" name="radiobuttons" value="b" id="radio-02"></input>
+  <label for="radio-02">Choice B</label>
+  <input type="radio" name="radiobuttons" value="c" id="radio-03"></input>
+  <label for="radio-03">Choice C</label>
+  ${args.tip ? tipDetailsTemplate(args) : ''}
+`;
+
+export const Radio = (args) => details.template(args, innerRadio);
+Radio.args = {
+  heading: 'Which choice would you prefer?',
+  helper: 'How can we help you?',
+  tip: 'By providing clarification on why this information is necessary.',
+  validation: '[&quot;isRequired&quot;]'
+};
+
+const innerSelect = (args) => `
+  <label slot="label">${args.label}</label>
+  <select name="select">
+    <option value="">Please select</option>
+    <option value="value-01">Value one</option>
+    <option value="value-02">Value two</option>
+    <option value="value-03">Value three</option>
+    <option value="value-04">Value four</option>
   </select>
 `;
 
-export const Select = (args) => details.template(args, selectInputText);
-Select.args = { label: 'A label', value: 'gas', validation: '[&quot;isRequired&quot;]' };
-
-const textareaInputText = (args) => `
-  <label slot="label" for="textarea-input">${args.label}</label>
-  <textarea id="textarea-input">${args.value}</textarea>
-`;
-
-export const Textarea = (args) => details.template(args, textareaInputText);
-Textarea.args = { label: 'A label', value: 'gas', validation: '[&quot;isRequired&quot;]' };
-
-const innerInputDate = (args) => `
-  <label slot="label">${args.label}</label>
-  <input type="text" value="${args.value}">
-`;
-export const Date = (args) => details.template(args, innerInputDate);
-Date.args = { label: 'A label', value: '', validation: '[&quot;isRequired&quot;,&quot;minDate(\'11/11/2021\')&quot;]' };
-
-const innerInputTel = (args) => `
-  <label slot="label">${args.label}</label>
-  <input type="tel" value="${args.value}" pattern="[0-9]{3}" title="match the pattern"/>
-`;
-
-export const Tel = (args) => details.template(args, innerInputTel);
-Tel.args = { label: 'A label', value: '', validation: '[&quot;isRequired&quot;]' };
+export const Select = (args) => details.template(args, innerSelect);
+Select.args = {
+  label: 'Select',
+  value: '',
+  validation: '[&quot;isRequired&quot;]'
+};
