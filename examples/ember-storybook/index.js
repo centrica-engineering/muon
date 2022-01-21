72ca57b6908f8a0ba7d66618533f430eb1d4c654
MekalaNagarajan-Centrica <mekala.nagarajan@centrica.com>
2022-01-21 13:14:02 +0000

    build: import more components from muon

--- examples/ember-storybook/index.js
+++ examples/ember-storybook/index.js
@@ -1,3 +1,11 @@
+import { Icon } from '@muons/library/components/icon';
 import { Cta } from '@muons/library/components/cta';
+import { Image } from '@muons/library/components/image';
+import { Inputter } from '@muons/library/components/inputter';
+import { Detail } from '@muons/library/components/detail';
 
+customElements.define('mn-icon', Icon);
 customElements.define('mn-cta', Cta);
+customElements.define('mn-image', Image);
+customElements.define('mn-inputter', Inputter);
+customElements.define('mn-detail', Detail);
