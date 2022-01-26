7da82395816efcefb7e4649d7bdf3844db06bccd
PraveenAsokan <praveen.asokan@centrica.com>
2022-01-26 21:12:17 +0000

    test: basic browserstack setup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress" />
-/// <reference types="@shelex/cypress-allure-plugin" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -12,19 +11,14 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-const cucumber = require('cypress-cucumber-preprocessor').default
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  on('file:preprocessor', cucumber())
-  allureWriter(on, config);
+
+  on('file:preprocessor', cucumber());
   return config;
-}
+
+};
 
 // module.exports = (on, config) => {
 //     allureWriter(on, config);
