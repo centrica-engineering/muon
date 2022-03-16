1ba320c8abc2ff1c9b33f7b46bd7e26485731098
MekalaNagarajan-Centrica <mekala.nagarajan@centrica.com>
2022-03-16 09:29:31 +0000

    chore: move image template

--- packages/library/components/card/src/card-component.js
+++ packages/library/components/card/src/card-component.js
@@ -1,14 +1,28 @@
-import { MuonElement, html } from '@muons/library';
+import { MuonElement, html, ScopedElementsMixin } from '@muons/library';
 import { CardMixin } from '@muons/library/mixins/card-mixin';
 import { ImageHolderMixin } from '@muons/library/mixins/image-holder-mixin';
+import { Image } from '@muons/library/components/image';
 
 /**
  * @element card
  */
-export class Card extends ImageHolderMixin(CardMixin(MuonElement)) {
+export class Card extends ScopedElementsMixin(ImageHolderMixin(CardMixin(MuonElement))) {
 
   constructor() {
     super();
+  }
+
+  static get scopedElements() {
+    return {
+      'card-image': Image
+    };
+  }
+
+  _imageTemplate() {
+    return this.image ? html`
+    <div class="media">
+      <card-image src=${this.image} alt=${this.alt}></card-image>
+    </div>` : html``;
   }
 
   get standardTemplate() {
--- packages/library/mixins/image-holder-mixin.js
+++ packages/library/mixins/image-holder-mixin.js
@@ -1,33 +1,17 @@
-import { html, dedupeMixin, ScopedElementsMixin } from '@muons/library';
-import { Image } from '@muons/library/components/image';
+import { dedupeMixin } from '@muons/library';
 
 export const ImageHolderMixin = dedupeMixin((superClass) =>
-  class ImageHolderdMixinClass extends ScopedElementsMixin(superClass) {
+  class ImageHolderdMixinClass extends superClass {
 
     static get properties() {
       return {
         image: {
-          type: String,
-          attribute: true
+          type: String
         },
         alt: {
-          type: String,
-          attribute: true
+          type: String
         }
       };
-    }
-
-    static get scopedElements() {
-      return {
-        'media-image': Image
-      };
-    }
-
-    _imageTemplate() {
-      return this.image ? html`
-      <div class="media">
-        <media-image src=${this.image} alt=${this.alt}></media-image>
-      </div>` : html``;
     }
   }
 );
