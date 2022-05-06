1cdd4dbdbbcecc38b3f517d91e1a64a926088b86
PraveenAsokan <Pravee.Asokan@centrica.com>
2022-05-06 14:22:06 +0100

    test: add variable for the repeated elements

--- cypress/integration/component_tests/muon_card.feature
+++ cypress/integration/component_tests/muon_card.feature
@@ -5,7 +5,7 @@ Feature: muon card component
 
         Given Launch the '<component>' component '<type>' type in the browser
         When the user changes the content in the card
-        # Then Validate the shadow dom and elements in '<type>' type
+        Then Validate the shadow dom and elements in '<type>' type
 
         Examples:
         |component       |type                  |
--- cypress/integration/component_tests/muon_card/muon_card.js
+++ cypress/integration/component_tests/muon_card/muon_card.js
@@ -11,34 +11,49 @@ Given('Launch the {string} component {string} type in the browser', (component, 
 When('the user changes the content in the card', () => {
     
     cy.document().then((doc)=>{
-       doc.querySelector('muon-card').querySelector('[slot="header"]').innerText = "Heading of the card component";
-       doc.querySelector('muon-card').querySelectorAll('p').item(0).innerText = "test the paragraph in card";
-       doc.querySelector('muon-card').querySelector('[slot="footer"]').innerText = "test the paragraph1 in card";
+
+       const cardComponent = doc.querySelector('muon-card');
+
+       cardComponent.querySelector('[slot="header"]').innerText = "Heading of the card component";
+       cardComponent.querySelector('p').innerText = "test the paragraph in card";
+       cardComponent.querySelector('[slot="footer"]').innerText = "test the paragraph1 in card";
     })
   
 });
 
 Then('Validate the shadow dom and elements in {string} type', (type) => {
 
+
   cy.document().then((doc)=>{
 
-     cy.get('muon-card').shadow().find('.card').should('exist');
-     cy.get('muon-card').shadow().find('.card').find('.body').should('exist');
+    cy.get('muon-card').shadow().then((cardShadow)=>{
+      cy.wrap(cardShadow).find('.card').should('exist');
+      cy.wrap(cardShadow).find('.card').find('.body').should('exist');
+    });
 
-     const header = doc.querySelector('muon-card').shadowRoot.querySelector('div[class="header"]').querySelector('slot[name="header"]').assignedNodes()[0].innerText;
-     const paragraph = doc.querySelector('muon-card').shadowRoot.querySelector('div[class="content"]').querySelector('slot').assignedNodes()[2].innerText;
-     const footer = doc.querySelector('muon-card').shadowRoot.querySelector('div[class="footer"]').querySelector('slot[name="footer"]').assignedNodes()[0].innerText;
+     const cardComponent = doc.querySelector('muon-card');
+
+     const header = cardComponent.shadowRoot.querySelector('div[class="header"]').querySelector('slot[name="header"]').assignedNodes()[0].innerText;
+     const paragraph = cardComponent.shadowRoot.querySelector('div[class="content"]').querySelector('slot').assignedNodes()[2].innerText;
+     const footer = cardComponent.shadowRoot.querySelector('div[class="footer"]').querySelector('slot[name="footer"]').assignedNodes()[0].innerText;
 
      assert.equal("Heading of the card component", header,'Header is different in shadowroot slot');
      assert.equal("test the paragraph in card", paragraph,'Paragraph is different in shadowroot slot');
      assert.equal("test the paragraph1 in card", footer,'Footer is different in shadowroot slot'); 
 
      if (type.includes('image')){
-      cy.get('muon-card').shadow().find('.card').find('.media').should('exist');
-      cy.get('muon-card').shadow().find('.card').find('.media').find('card-image').invoke('attr','src').should('eq','https://www.britishgas.co.uk/aem6/content/dam/britishgas/images/smart-meters/Technology/Lockup%202.png');
-      cy.get('muon-card').shadow().find('.card').find('.media').find('card-image').find('div.image').find('img').invoke('attr','src').should('eq','https://www.britishgas.co.uk/aem6/content/dam/britishgas/images/smart-meters/Technology/Lockup%202.png');
-      cy.get('muon-card').shadow().find('.card').find('.media').find('card-image').find('div.image').find('img').invoke('attr','class').should('eq','blur-out image-lazy');
-      cy.get('muon-card').shadow().find('.card').find('.media').find('card-image').invoke('attr','alt').should('be.empty');
+
+        cy.get('muon-card').shadow().find('.card').find('.media').then((cardMedia)=>{
+          cy.wrap(cardMedia).should('exist');
+
+          cy.wrap(cardMedia).find('card-image').then((cardImage)=>{
+            cy.wrap(cardImage).invoke('attr','src').should('eq','https://www.britishgas.co.uk/aem6/content/dam/britishgas/images/smart-meters/Technology/Lockup%202.png');
+            cy.wrap(cardImage).find('div.image').find('img').invoke('attr','src').should('eq','https://www.britishgas.co.uk/aem6/content/dam/britishgas/images/smart-meters/Technology/Lockup%202.png');
+            cy.wrap(cardImage).find('div.image').find('img').invoke('attr','class').should('eq','blur-out image-lazy');
+            cy.wrap(cardImage).invoke('attr','alt').should('be.empty');
+          })
+          
+        })
      }
   })
 
