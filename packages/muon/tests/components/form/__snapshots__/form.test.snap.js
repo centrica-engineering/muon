c6fd41117c5201218721bc0c2501a5e4fca3fb93
MekalaNagarajan-Centrica <mekala.nagarajan@centrica.com>
2022-06-29 14:19:52 +0100

    fix: test failures

--- packages/muon/tests/components/form/__snapshots__/form.test.snap.js
+++ packages/muon/tests/components/form/__snapshots__/form.test.snap.js
@@ -2,58 +2,44 @@
 export const snapshots = {};
 
 snapshots["form implements standard self [with form]"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form implements standard self [with form] */
 
 snapshots["form form with submit button"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form with submit button */
 
 snapshots["form form with submit input"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form with submit input */
 
 snapshots["form form with reset button"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form with reset button */
 
 snapshots["form form with reset input"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form with reset input */
 
 snapshots["form form submitting"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form submitting */
 
 snapshots["form form button disabled submitting"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form button disabled submitting */
 
@@ -66,42 +52,32 @@ snapshots["form form submitting validation"] =
 /* end snapshot form form submitting validation */
 
 snapshots["form form submitting with input"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form submitting with input */
 
 snapshots["form form submitting with inputter"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form submitting with inputter */
 
 snapshots["form form submitting with inputter parent"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form submitting with inputter parent */
 
 snapshots["form form with reset cta"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form with reset cta */
 
 snapshots["form form with submit cta"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form with submit cta */
 
@@ -114,26 +90,20 @@ snapshots["form form button loading submitting"] =
 /* end snapshot form form button loading submitting */
 
 snapshots["form form cta loading submitting"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form cta loading submitting */
 
 snapshots["form form cta loading reset"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form cta loading reset */
 
 snapshots["form form submitting with inputter [validate]"] = 
-`<div class="form">
-  <slot>
-  </slot>
-</div>
+`<slot>
+</slot>
 `;
 /* end snapshot form form submitting with inputter [validate] */
 
