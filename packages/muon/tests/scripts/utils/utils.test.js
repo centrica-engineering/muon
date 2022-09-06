af68a2e64527889058131ab2bf0fe90a50512245
MekalaNagarajan-Centrica <mekala.nagarajan@centrica.com>
2022-09-06 16:09:16 +0100

    test: more tests

--- packages/muon/tests/scripts/utils/utils.test.js
+++ packages/muon/tests/scripts/utils/utils.test.js
@@ -1,4 +1,5 @@
 const testRunner = require('ava');
+const sinon = require('sinon');
 
 let utilsLibrary;
 testRunner.before(async () => {
@@ -30,4 +31,32 @@ testRunner('filterPathToCustomElements single component in array', async (t) => 
 testRunner('filterPathToCustomElements multiple component', async (t) => {
   const componentList = await utilsLibrary.filterPathToCustomElements(['inputter', 'image']);
   t.is(componentList, '{inputter,image}');
+});
+
+testRunner('getConfig default file and root', async (t) => {
+  const config = utilsLibrary.getConfig();
+  t.true(config !== undefined);
+});
+
+testRunner('getConfig new file default root', async (t) => {
+  const config = utilsLibrary.getConfig('browserstack.json');
+  t.true(config !== undefined);
+});
+
+testRunner('getConfig config file not exist', async (t) => {
+  sinon.stub(process, 'exit');
+  utilsLibrary.getConfig('test.json');
+  t.true(process.exit.called);
+  t.true(process.exit.calledWith(1));
+});
+
+testRunner('getConfig config file', async (t) => {
+  const config = utilsLibrary.getConfig('tests/scripts/utils/test.json');
+  t.true(config !== undefined);
+});
+
+testRunner('findComponents', async (t) => {
+  const componentsList = await utilsLibrary.findComponents();
+  t.true(componentsList !== undefined);
+  t.true(componentsList.includes(process.cwd() + '/components/card/src/card-component.js'));
 });
