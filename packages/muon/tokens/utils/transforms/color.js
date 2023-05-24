847152be63465de4174926a0c971f860234f29e2
jholt1 <jonathan.holt@centrica.com>
2023-05-24 12:30:40 +0100

    chore: allow additional parameters color transform

--- packages/muon/tokens/utils/transforms/color.js
+++ packages/muon/tokens/utils/transforms/color.js
@@ -5,14 +5,28 @@ const colorTransform = (token) => {
   let color = chroma(value);
   // iterate over the modify array (see tokens/color.json)
   // and apply each modification in order
-  modify.forEach(({ type, amount }) => {
+  modify.forEach(({ type, amount, ratio, mode }) => {
     // modifier type must match a method name in chromajs
     // https://gka.github.io/chroma.js/
     // chroma methods can be chained, so each time we override the color variable
     // we can still call other chroma methods, similar to
     // chroma(value).brighten(1).darken(1).hex();
-    color = color[type](amount);
 
+    // if the modifier type is 'mix', we need to pass in the color to mix with
+    // and the ratio to mix by
+    if (type === 'mix') {
+      color = color.mix(amount, ratio);
+    }
+
+    // if the modifier type is 'blend', we need to pass in the color to blend with
+    // and the mode to blend by
+    if (type === 'blend') {
+      color = color.blend(amount, mode);
+    }
+
+    if (type !== 'mix' && type !== 'blend') {
+      color = color[type](amount);
+    }
   });
 
   return color.hex();
