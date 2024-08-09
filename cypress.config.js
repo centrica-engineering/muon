ce58fba638305629a24de9587207ab8a96aabc7e
MekalaNagarajan_Centrica <mekala.nagarajan@centrica.com>
2024-08-09 11:27:44 +0100

    test: simplify the env logic

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,29 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 const { allureCypress } = require("allure-cypress/reporter");
-const data = require("./config/base-config.json");
 
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config){
+    async setupNodeEvents(on, config) {
       await preprocessor.addCucumberPreprocessorPlugin(on, config);
-      on('file:preprocessor', 
-      createBundler({
-        plugins: [createEsbuildPlugin.default(config)],
-      })
-    )
+      on('file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin.default(config)]
+        })
+      );
       allureCypress(on, {
-        resultsDir: "./allure-results",
+        resultsDir: "./allure-results"
       });
       return config;
     },
-    env : {
-      tags: data.env.tags,
+    env: {
+      tags: '@card or @cta or @detail or @form or @image or @icon or @inputter or @cake'
     },
     includeShadowDom: true,
-    experimentalRunAllSpecs : true,
+    experimentalRunAllSpecs: true,
     supportFile: 'cypress/support/e2e.js',
-    specPattern: ['cypress/e2e/component_tests/*.feature','cypress/e2e/example_app_tests/*.feature'],
+    specPattern: ['cypress/e2e/component_tests/*.feature', 'cypress/e2e/example_app_tests/*.feature'],
   },
 })
