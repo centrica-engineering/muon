03c9d868eef10b5d1cb5ad856c57cdb3040e2cdb
MekalaNagarajan_Centrica <mekala.nagarajan@centrica.com>
2026-01-21 09:45:17 +0000

    build: upgrade storybook 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
-const json = require('@rollup/plugin-json');
-const stories = require('@muonic/muon/storybook/find-stories');
+import json from '@rollup/plugin-json';
+import stories from '@muonic/muon/storybook/find-stories';
+import deepmerge from 'deepmerge';
 
+/** @type {import('@web/storybook-framework-web-components').StorybookConfig} */
 const findStories = async () => {
   const muonStories = await stories(__dirname);
   return [
@@ -11,7 +13,17 @@ const findStories = async () => {
 
 module.exports = {
   stories: async () => await findStories(),
-  async rollupConfig(config) {
+  framework: {
+    name: '@web/storybook-framework-web-components'
+  },
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y'
+  ],
+  docs: {
+    autodocs: true
+  },
+  async rollupFinal(config) {
     const { rollupPlugins } = await import('@muonic/muon/scripts/rollup-plugins.mjs');
 
     const plugins = config.plugins.map((plugin) => {
@@ -27,5 +39,11 @@ module.exports = {
     ];
 
     return config;
+  },
+  async wdsFinal(config) {
+    const serverConfig = await import('@muonic/muon/storybook/server.config.mjs');
+    const StorybookConfig = serverConfig.default;
+
+    return deepmerge(config, StorybookConfig);
   }
 }
