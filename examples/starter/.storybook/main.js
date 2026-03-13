e84cb76b7bb6dab9587ef33120afd42046e9aa7b
MekalaNagarajan_Centrica <mekala.nagarajan@centrica.com>
2026-03-13 11:01:46 +0000

    build: update example apps storybook

--- examples/starter/.storybook/main.js
+++ examples/starter/.storybook/main.js
@@ -1,6 +1,5 @@
-const json = require('@rollup/plugin-json');
 const stories = require('@muonic/muon/storybook/find-stories');
-const deepmerge = require('deepmerge');
+const config = require('@muonic/muon/storybook/storybook.config');
 
 const findStories = [
     '../components/**/story.@(js|jsx|ts|tsx)',
@@ -10,37 +9,5 @@ const findStories = [
 
 module.exports = {
   stories: [...findStories],
-    framework: {
-    name: '@web/storybook-framework-web-components'
-  },
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y'
-  ],
-  docs: {
-    autodocs: true
-  },
-  async rollupFinal(config) {
-      const { rollupPlugins } = await import('@muonic/muon/scripts/rollup-plugins.mjs');
-  
-      const plugins = config.plugins.map((plugin) => {
-        if (plugin.name !== 'babel') {
-          return plugin;
-        }
-      }).filter(plugin => plugin);
-  
-      config.plugins = [
-        json(),
-        ...rollupPlugins,
-        ...plugins
-      ];
-  
-      return config;
-    },
-    async wdsFinal(config) {
-      const serverConfig = await import('@muonic/muon/storybook/server.config.mjs');
-      const StorybookConfig = serverConfig.default;
-  
-      return deepmerge(config, StorybookConfig);
-    }
+  ...config
 };
